e2b9ce3cb3c369c3c1724bdb34c7f84ef4def550
Karina <kcastillouribe@gmail.com>
2022-08-06 19:49:13 -0400

    Se agrego stylo a card con color waening.

--- assets/js/viajes.js
+++ assets/js/viajes.js
@@ -4,8 +4,19 @@ $(function () {
 
 });
 
+$('.change').click(function(){
+    $(this).removeClass("bg-info");
+    $(this).addClass("bg-warning");
+ })
+
+$('.change').dblclick(function(){
+    $(this).removeClass("bg-warning");
+    $(this).addClass("bg-info");
+ })
+
 $(document).scroll(function (){
     const y = $('html').scrollTop();
 
     y > 300 ? $('nav').addClass('nav-black') : $('nav').removeClass("nav-black");
 });
+
